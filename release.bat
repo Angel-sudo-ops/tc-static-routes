af8ad65688c7023b73001bd2b43f9f25be8a554c
Jose Contreras <contreras.j@e80group.com>
2026-04-19 13:40:04 -0500

    adjust release and changelog logic

--- release.bat
+++ release.bat
@@ -11,25 +11,6 @@ REM Executable and version paths
 set EXE=dist\StaticRoutesCreator.exe
 set VERSION_FILE=version.txt
 
-REM Generate changelog 
-git describe --tags --abbrev=0 > prev_tag.tmp 2>nul
-set /p PREV_TAG=<prev_tag.tmp
-
-echo [%VERSION%] > changelog.new
-
-(
-git log %PREV_TAG%..HEAD --pretty=format:"- %%s" 
-) | findstr /v /i "export .exe" >> changelog.new
-
-echo. >> changelog.new
-
-if exist changelog.txt (
-    type changelog.txt >> changelog.new
-)
-
-move /Y changelog.new changelog.txt
-del prev_tag.tmp
-
 REM log path
 set LOG=changelog.txt
 
